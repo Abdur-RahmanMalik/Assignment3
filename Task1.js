b1ea8e7f4886b64074876a857faa28c774e14696
Abdur-RahmanMalik <76841813+Abdur-RahmanMalik@users.noreply.github.com>
2023-03-28 00:24:04 +0500

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,4 @@
+let age = 25;
+let ageAsString = age.toString();
+console.log("Value: ",ageAsString)
+console.log("Data Type: ",typeof ageAsString)
---
+++ Task4.js
@@ -0,0 +1,8 @@
+let name = "MALIK"
+let age = 23
+
+if(age >= 18) {
+  console.log("Welcome " + name + ", you are an adult.");
+} else {
+  console.log("Sorry " + name + ", you are not yet an adult.");
+}
